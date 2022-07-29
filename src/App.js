8ba705e6d344f73406aada9200d71bf452b0f704
Carlos Brito <carlos.e1998g@gmail.com>
2022-07-29 14:02:07 -0400

    src content

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+import About from './components/about/About';
+import Cover from "./components/cover/Cover";
+import Footer from './components/footer/Footer';
+import Info from './components/info/Info';
+import Loader from './components/Loader/Loader';
+import Navbar from './components/navbar/Navbar';
+import Scrollbar from './components/Scrollbar/Scrollbar';
+import Skills from './components/skills/Skills';
+import Slider from './components/slider/Slider';
+
+function App() {
+/*----------------------------------------------------------SCROLL NAVBAR*/ 
+const [scrollTop, setScrollTop] = useState(0);
+const onScroll = () => {
+  const winScroll = document.documentElement.scrollTop;
+  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+  const scrolled = (winScroll/height) * 100;
+  setScrollTop(scrolled); 
+}
+
+useEffect(() => {
+  window.addEventListener("scroll", onScroll);
+
+  return () =>  window.removeEventListener("scroll", onScroll);
+}, []);
+
+/*----------------------------------------------------------loading*/
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false);
+    }, 3500);
+  }, []);
+
+
+  /*-----------------------------------------------------------PRUEBAS */
+
+  
+
+  return (
+    <>
+    {loading ? (
+      <Loader />
+    ) : (
+      <div className="App">
+
+        <Navbar isScrolling={scrollTop} />
+
+        
+        {/*<Scrollbar isScrolling={scrollTop} />*/}
+        
+      
+        <Cover />
+
+        <About />
+
+        <Slider />
+
+        <Skills />
+
+        <Info />
+
+        <Footer/>
+      </div>
+    )}
+  </>
+  );
+}
+
+export default App;
